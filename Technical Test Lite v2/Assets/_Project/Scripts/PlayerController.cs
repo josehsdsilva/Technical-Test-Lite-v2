63f18386419ffddf49d3c1849aca0e7deb2f4cf0
José Silva <josehsdsilva@gmail.com>
2022-03-15 00:30:57 +0000

    First commit;

---
+++ Technical Test Lite v2/Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // ToDo: usar novo input system
+    }
+}
